48e758152706165247affcd82d0c6b903069c380
Thibaud Colas <thibaudcolas@gmail.com>
2017-11-08 15:56:49 +0200

    Change how data is loaded into app to support offline loading

--- generate.py
+++ generate.py
@@ -50,7 +50,7 @@ if __name__ == '__main__':
         else:
             app['supports_py3'] = False
 
-    with codecs.open('./public/data.js', 'w', 'utf-8') as file:
-        file.write("window.data = %s;" % json.dumps({
+    with codecs.open('./src/data.js', 'w', 'utf-8') as file:
+        file.write("export default %s;" % json.dumps({
             'apps': apps,
         }, indent=True))
--- src/index.js
+++ src/index.js
@@ -5,24 +5,23 @@ import registerServiceWorker from './registerServiceWorker';
 import App from './App';
 import notes from './notes';
 import pypinfo from './pypinfo';
+import data from './data';
 
 import './index.css';
 
-const apps = window.data.apps.map(a => {
-    const downloads = pypinfo[a.pypi_package_name]
-        ? pypinfo[a.pypi_package_name].reduce(
-              (sum, count) => sum + count.download_count,
-              0,
-          )
+const apps = data.apps.map(a => {
+    const name = a.pypi_package_name;
+    const downloads = pypinfo[name]
+        ? pypinfo[name].reduce((sum, count) => sum + count.download_count, 0)
         : null;
 
     return Object.assign(
         a,
         {
             downloads: downloads,
-            detailed_downloads: pypinfo[a.pypi_package_name],
+            detailed_downloads: pypinfo[name],
         },
-        notes[a.pypi_package_name],
+        notes[name],
     );
 });
 
